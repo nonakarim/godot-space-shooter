ebe4b9fbddec532ff58b7272d372fe525a275807
Hana Karim <nona.karim25@gmail.com>
2026-03-27 15:08:57 +0200

    Add laser growth as it goes up (Every 0.5 seconds)

--- Scripts/laser.gd
+++ Scripts/laser.gd
@@ -1,4 +1,10 @@
 extends Area2D
 @export var speed = 500
+
+func _ready() -> void:
+	$LaserImage.scale = Vector2(0, 0)
+	var tween = create_tween()
+	tween.tween_property($LaserImage, 'scale', Vector2(1, 1), 0.5)
+	
 func _process(delta: float) -> void:
 	position.y -= speed * delta

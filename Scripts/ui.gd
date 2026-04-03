f1dc9fe267df33dcda7fd89c8a989ddafa9cbb6f
Hana Karim <nona.karim25@gmail.com>
2026-04-03 15:15:44 +0200

    Update lives size and place

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 static var image = load("res://Graphics/PNG/playerShip2_orange.png")
 
+
 func set_health(amount):
 	#Remove all children
 	for child in $MarginContainer2/HBoxContainer.get_children():
@@ -11,12 +12,19 @@ func set_health(amount):
 	for i in amount:
 		var texRect = TextureRect.new()
 		texRect.texture = image
+		texRect.custom_minimum_size = Vector2(50, 50)
+		
+		texRect.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+		texRect.size_flags_vertical = Control.SIZE_SHRINK_CENTER
+		
+		texRect.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+		
+		texRect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
 		
 		$MarginContainer2/HBoxContainer.add_child(texRect)
-		texRect.stretch_mode = TextureRect.STRETCH_KEEP
 		
 func set_score(score): 
 	$MarginContainer/Label.text = "Score: " + str(score)
 	
 func set_laser_count(laser):
-	$MarginContainer3/Label.text = "Lasers left: " + str(laser)
+	$MarginContainer3/Label.text = "Lasers: " + str(laser)

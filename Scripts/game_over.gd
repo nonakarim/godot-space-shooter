7a43784a7b7392960a9f527596fc853f53528a5b
Hana Karim <nona.karim25@gmail.com>
2026-04-08 13:50:25 +0200

    Change music in game over scene from game play musi to lose music

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -4,8 +4,12 @@ func _ready() -> void:
 	$Score.text += str(Global.score)
 	Global.laserCount = 10
 	Global.health = 3
+	Music.playing = false
+	$AudioStreamPlayer2D.play()
 
 func _process(delta: float) -> void:
+	Music.playing = false
+		
 	if Input.is_action_just_pressed("Replay"):
 		get_tree().change_scene_to_file("res://Scenes/level.tscn")
 	elif Input.is_action_just_pressed("exit"):
